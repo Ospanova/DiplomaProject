229133d07a5d1bb39fe9b5ef9b2139d9c2a4a9cf
Aida Ospanova <ospanovaida08@gmail.com>
2020-05-21 22:56:25 +0500

    Added docker

--- cinema/settings.py
+++ cinema/settings.py
@@ -104,23 +104,23 @@ WSGI_APPLICATION = 'cinema.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aida',
-        'USER': 'aida',
-        'PASSWORD': 'aida',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'aida',
+#         'USER': 'aida',
+#         'PASSWORD': 'aida',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
